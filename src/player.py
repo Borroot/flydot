65dfd2c5cb4f26a2b4cf07ce61408f96b7aa001b
Borroot <borroot@live.nl>
2020-11-03 20:29:10 +0100

    Add player movement over y axis.

--- src/player.py
+++ src/player.py
@@ -6,8 +6,7 @@ from vector import *
 
 class Player:
 
-    VELX_MAX = 10
-    ACCX_MAX = 8
+    VELX_MAX = 3
 
     SIZE = (40, 40)
 
@@ -15,7 +14,6 @@ class Player:
     def __init__(self):
         self.pos = (SIZE_GAME[0] // 8, SIZE_GAME[1] // 2)
         self.vel = (0, 0)
-        self.acc = (0, 0)
 
         self.surface = pygame.Surface(Player.SIZE)
         self.rect = self.surface.get_rect(center = (self.pos))
@@ -31,22 +29,13 @@ class Player:
 
 
     def move_x(self, dirr, pipes):
-        if (dirr == MOVE_L and self.acc[0] > 0) or (dirr == MOVE_R and self.acc[0] < 0):
-            self.vel = (0, self.vel[1])
-            self.acc = (0, self.acc[1])
+        self.vel = ((-1 if dirr==MOVE_L else 1) * Player.VELX_MAX, self.vel[1])
+        self.pos = add(self.pos, self.vel)
+        self.rect.center = self.pos
 
-        self.acc = (-3 if dirr == MOVE_L else 3, self.acc[1])
-        if dirr == MOVE_L:
-            self.acc = (max(self.acc[0], -Player.ACCX_MAX), self.acc[1])
-        if dirr == MOVE_R:
-            self.acc = (min(self.acc[0],  Player.ACCX_MAX), self.acc[1])
 
-        self.vel = add(self.vel, self.acc)
-        if dirr == MOVE_L:
-            self.vel = (max(self.vel[0], -Player.VELX_MAX), self.vel[1])
-        if dirr == MOVE_R:
-            self.vel = (min(self.vel[0],  Player.VELX_MAX), self.vel[1])
-
+    def move_y(self, dirr, pipes):
+        self.vel = (self.vel[0], -10 if dirr == MOVE_U else self.vel[1] + 0.5)
         self.pos = add(self.pos, self.vel)
         self.rect.center = self.pos
 
@@ -54,3 +43,5 @@ class Player:
     def move(self, dirr, pipes):
         if dirr == MOVE_L or dirr == MOVE_R:
             self.move_x(dirr, pipes)
+        if dirr == MOVE_U or dirr == MOVE_D:
+            self.move_y(dirr, pipes)
